f91856f68aef3d85a0557797aee74c4956db921e
vesax1 <lehttu21@rpkk.fi>
2022-10-21 08:39:17 +0300

    first commit

---
+++ 1710_teht/app.js
@@ -0,0 +1,12 @@
+let text = ""
+let text2 = ""
+
+for (let i = 1; i <= 100; i++) {
+    text += i + "\n\r" ;
+}
+console.log(text);
+
+for (let i = 2; i <= 100; i+2) {
+    text2 += i + "\n\r" ;
+}
+console.log(text2);
---
+++ yhteenlaskut/app.js
@@ -0,0 +1,6 @@
+let summa = 0
+
+for (let i = 1; i < 100; i=i+2){
+    summa = summa + i ** i
+}
+console.log(summa)
